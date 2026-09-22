1891bf034cf51d2bc548820a8b2d58c7a8a6fb45
Kyle <nettervillekyle@gmail.com>
2026-09-22 15:53:45 -0500

    feat: Created folder structures for team

---
+++ lib/game/dyno_game.dart
@@ -0,0 +1,14 @@
+import 'package:flame/game.dart';
+
+   enum GameState { intro, playing, gameOver }
+
+   class DynoGame extends FlameGame {
+     GameState state = GameState.intro;
+
+     @override
+     Future<void> onLoad() async {
+       // DaveyBoll will add Player component here
+       // Justinat0r will add Spawner & Background here
+       // spvvn will hook up HUD overlays here
+     }
+   }
